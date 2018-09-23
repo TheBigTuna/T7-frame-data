01ba2f4f5d498aab3eb8517fb96a2fbe4e2018cf
octavius <mooreoctavius94@gmail.com>
2018-09-23 08:30:55 -0400

    9/23/2018

--- scripts/regular.js
+++ scripts/regular.js
@@ -1,10 +1,10 @@
-
+let characterArray = [];
 
 let sourceJs ={
 
 scriptsJs: () => {
 
-    scriptsName = ["akuma", "alisa", "asuka", "bryan",
+    scriptsName = ["akuma", "alisa", "asuka", "bob", "bryan",
          "claudio", "devil-jin", "dragunov", "eliza",
           "heihachi", "hwoarang", "jack-7", "jin", 
          "josie", "katarina", "kazumi", "kazuya", "king", "lars", 
@@ -27,6 +27,9 @@ charList: () => {
          law, lee, leo, lili, ling, luckyChloe, masterRaven, 
          miguel, nina, paul, shaheen, steve, yoshimitsu]; 
 
+         characterArray.push(char);
+         console.log(characterArray)
+
     let charDetails = char.map((list) => {
         let name = list.metadata.name;
         let game = list.metadata.game;
@@ -46,6 +49,7 @@ charList: () => {
             moves: moves,
             character: character,
             gifs: gifs,
+            char: char
             
         } 
         });
@@ -59,15 +63,19 @@ charList: () => {
 
             
                         
-            displayAkuma = () => {
+            displayMoves = (b,c) => {
                 // console.log(akuma.moves.length)
-                for(a=0; a < akuma.moves.length; a++){
-                    // console.log(akuma.moves[a]);
-                    // $('.char0').append("<p class='notation'>" + akuma.moves[a].notation + "</p>")
-                    // $('.char0').append("<p class='hit-level'>" + akuma.moves[a].hit_level + "</p>")
-                    // $('.char0').append("<p class='damage'>" + akuma.moves[a].damage + "</p>")
+                for(a=0; a < b.moves.length; a++){
+                $(document).find('.body-box' + c).append("<p class='notation'>" + b.moves[a].notation + "</p>");
+                $(document).find('.body-box' + c).append("<p class='hit-level'>" + b.moves[a].hit_level + "</p>");
+                $(document).find('.body-box' + c).append("<p class='damage'>" + b.moves[a].damage + "</p>");
+                $(document).find('.body-box' + c).append("<p class='on-block'>" + b.moves[a].on_block + "</p>");
+                $(document).find('.body-box' + c).append("<p class='on-ch'>" + b.moves[a].on_ch + "</p>");
+                $(document).find('.body-box' + c).append("<p class='on-hit'>" + b.moves[a].on_hit + "</p>");
+            
                 }
-             console.log(akuma.metadata.name);
+             $(document).find('.header-box' + c).append("<p>" + b.metadata.name + "</p>");
+             console.log(b);
             }
 
 
@@ -141,24 +149,31 @@ charList: () => {
             //  $('.appChar').append("<ul class = 'objList'></ul>");
             //  $('.appChar').append("<li class = 'charMoves list-group-item'>Moves</li>");
             //  $('.appChar').append("<li class = 'charGifs list-group-item'>Gifs</li>");
-                let charMovesValue;
+                let charHeadValue;
+                let charBodyValue;
 
              $('.charMoves').click(function (){
-                    console.log(this);
-                    console.log(charMovesValue);
-                    charMovesValue = this.value;
+                    // console.log(this);
+                    // console.log(charHeadValue);
+                    charHeadValue = this.value;
+                    charBodyValue = this.value;
                     $('.modal-background').css('display', 'block');
-                    $('.modal-header').addClass("box" + this.value);
+                    $('.modal-header').addClass("header-box" + this.value);
+                    $('.modal-body').addClass("body-box" + this.value);
+                    displayMoves(characterArray[0][this.value],this.value);
+                   
 
              });
 
               $('.charGifs').click(function (){
                     console.log(this);
              });
-                $('.modal-close').click(function (){
-                    console.log(charMovesValue);
+                $('.modal-close').click(function (){                
                     $('.modal-background').css('display', 'none');
-                    $('.modal-header').removeClass("box" + charMovesValue);
+                    $('.modal-header').removeClass("header-box" + charHeadValue);
+                    $('.modal-header p').remove();
+                    $('.modal-body').removeClass("body-box" + charBodyValue);
+                    $('.modal-body p').remove();
              });
 
             //  $('.appChar').on('click', function () {
@@ -184,8 +199,6 @@ charList: () => {
 init: () => {
        sourceJs.scriptsJs();
        setTimeout( ()  => { sourceJs.charList(); }, 1000);
-       setTimeout( ()  => {displayAkuma(); }, 1100);
-
        setTimeout( ()  => { sourceJs.ui(); }, 1200);
 },
 };
