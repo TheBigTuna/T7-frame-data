47438149dd4b67c643ce3842620eaa408f630d01
octavius <mooreoctavius94@gmail.com>
2018-09-23 00:10:31 -0400

    update 9/23/2018

--- scripts/regular.js
+++ scripts/regular.js
@@ -50,13 +50,42 @@ charList: () => {
         } 
         });
 
+        let charArray = [];
+        let movesArray = [];
+
+
+
+
+
+            
+                        
+            displayAkuma = () => {
+                // console.log(akuma.moves.length)
+                for(a=0; a < akuma.moves.length; a++){
+                    // console.log(akuma.moves[a]);
+                    // $('.char0').append("<p class='notation'>" + akuma.moves[a].notation + "</p>")
+                    // $('.char0').append("<p class='hit-level'>" + akuma.moves[a].hit_level + "</p>")
+                    // $('.char0').append("<p class='damage'>" + akuma.moves[a].damage + "</p>")
+                }
+             console.log(akuma.metadata.name);
+            }
+
+
+
+
+
+
         for (i = 0; i < charDetails.length; i++) {
             // Entire Char List
 
             // console.log(charDetails[i])  
-            $("#appList").append("<div id = " + charDetails[i].character + " class = 'appChar list-group-item'>" + "<h4>" + charDetails[i].name + "</h4>" + "</div>");
-
+             $("#appList").append("<div id = " + charDetails[i].character + " class = 'appChar off char"+ [i] + " list-group-item' value = " + [i] + ">" + "<h4>" + charDetails[i].name + " " + "<span class='game'>" + charDetails[i].game + "</span></h4>" + "</div>");
+             $('#appList').append("<ul class = 'objList' value = " + [i] + "></ul>");
+             $('#appList').append("<li class = 'charMoves list-group-item' value = " + [i] + ">Moves</li>");
+             $('#appList').append("<li class = 'charGifs list-group-item' value = " + [i] + ">Gifs</li>");
+             charArray.push(charDetails[i]);
         }
+
             // Single Char
 
             //  $('.charMoves').on('click', (e) => {
@@ -65,12 +94,35 @@ charList: () => {
             // });
             // <----------------------------------------------------------------------------------------------------------------------------------->
 
+            
             // grabMoves = (c) => {
             // for (a = 0; a < charDetails[c].moves.length; a++){
-            //     console.log(charDetails[c].moves[a]);
+            //      console.log(charDetails[c].moves[a]);
+                
             // }
             // }
-            // grabMoves();
+            // grabMoves(2);
+            // for(a=0; a < charArray.length; a++){
+            //     movesArray.push(charArray[a].moves);
+            // }
+            
+            // displayMoves = (c) => {
+            //     for(b = 0; b < movesArray.length; b++)
+            //     console.log(movesArray[b]);
+            //     // $('.char0').append(movesArray[b][c].notation)
+            // }
+            // displayMoves(0);
+        
+             
+            // displayMoves = (c) => {
+            //     for(b = 0; b < movesArray.length; b++)
+            //     console.log(movesArray[b][c]);
+            // }
+            // displayMoves(c);
+        
+
+
+
 },
     load: () => {
             $("#loading").fadeOut(1000);
@@ -86,30 +138,54 @@ charList: () => {
 
 },  
     ui: () => {
-            
-             $('.appChar').on('click', function (a) {
-                 let check = $(this).hasClass('on');
-                 $(this).css("display", "block");
-                 $(this).addClass('on');
-                 console.log(this);
-                 console.log(check);
-                 
-            //  $(".appChar").append("<ul></ul>");
-            //  $(".appChar ul").append("<li class = 'charMoves'>Moves</li>");
-            //  $(".appChar ul").append("<li class = 'charGifs'>Gifs</li>");
+            //  $('.appChar').append("<ul class = 'objList'></ul>");
+            //  $('.appChar').append("<li class = 'charMoves list-group-item'>Moves</li>");
+            //  $('.appChar').append("<li class = 'charGifs list-group-item'>Gifs</li>");
+                let charMovesValue;
 
-             if (check == true){
-                 $(this).removeClass('on');
-                 $(this).addClass('off');
-                 console.log('hi')
-             }
+             $('.charMoves').click(function (){
+                    console.log(this);
+                    console.log(charMovesValue);
+                    charMovesValue = this.value;
+                    $('.modal-background').css('display', 'block');
+                    $('.modal-header').addClass("box" + this.value);
+
              });
+
+              $('.charGifs').click(function (){
+                    console.log(this);
+             });
+                $('.modal-close').click(function (){
+                    console.log(charMovesValue);
+                    $('.modal-background').css('display', 'none');
+                    $('.modal-header').removeClass("box" + charMovesValue);
+             });
+
+            //  $('.appChar').on('click', function () {
+            //      let check = $(this).hasClass('on');
+            // if (check == false){
+            //     $(this).addClass('on');
+            //     $(this).removeClass('off');
+            //      console.log(this);
+            //  $(this).append("<ul class = 'objList'></ul>");
+            //  $(this).append("<li class = 'charMoves list-group-item''>Moves</li>");
+            //  $(this).append("<li class = 'charGifs list-group-item''>Gifs</li>");
+            //  }
+            // if (check == true){
+            //      $(this).removeClass('on');
+            //      $(this).addClass('off');
+            //      $(this).empty();
+            //  }   
+
+            //  });
              
     },
 
 init: () => {
        sourceJs.scriptsJs();
        setTimeout( ()  => { sourceJs.charList(); }, 1000);
+       setTimeout( ()  => {displayAkuma(); }, 1100);
+
        setTimeout( ()  => { sourceJs.ui(); }, 1200);
 },
 };
